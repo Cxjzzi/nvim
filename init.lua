8faeda5264d5e7da901703456e9fce8442e2e7c5
Crissy.E <crissy.erebua@ontariotechu.net>
2026-01-29 16:19:35 -0500

    Made changes

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+-- Editing Settings
 vim.opt.number = true -- Gives me line numbers
 vim.opt.cursorline = true --Gives me the wondering highlight
 vim.opt.wrap = true --Lets the words wrap rather then go off screen
@@ -16,5 +17,17 @@ vim.opt.spell = true --Spellcheck on comments and text
 
 vim.opt.clipboard = "unnamedplus" -- Changes program clipboard to windows clipboard
 
+-- vim.opt.statusline
 
+vim.opt.splitright = true -- New vertical splits go to the right
+vim.opt.splitbelow = true -- New horizontal splits go below
 
+-- Adding Plug In
+
+vim.pack.add({
+   {
+      src = "https://github.com/github/copilot.vim"
+   }
+})
+   
+--
